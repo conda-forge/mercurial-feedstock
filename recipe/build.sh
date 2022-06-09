be05185d5f135213962873c996d4f18c5606e62e
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-06-09 11:16:58 +0200

    Fix build with Rust

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,11 @@
 
 export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,$CONDA_PREFIX/lib -C link-arg=-Wl,-rpath,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
 
-$PYTHON -m pip install --no-deps --ignore-installed -vv --global-option "--rust" .
+{ # try
+$PYTHON -m pip install --no-deps --ignore-installed -vv --no-use-pep517 --global-option --rust .
+} || { # catch
+echo "ls rust/target/release/*" && \
+  ls rust/target/release/* && \
+  echo "ls rust/target/release" && \
+  ls rust/target/release
+}
