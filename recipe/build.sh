107c8466ccab714aa921270b385a7e627e014670
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-06-09 21:36:50 +0200

    Patch setup.py to honor CARGO_BUILD_TARGET

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,12 @@
 
 export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,$CONDA_PREFIX/lib -C link-arg=-Wl,-rpath,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
 
+sed -i'' "s/os.path.join('rust', 'target', 'release')/os.path.join('rust', 'target', os.environ.get('CARGO_BUILD_TARGET', ''), 'release')/" setup.py
+
 { # try
 $PYTHON -m pip install --no-deps --ignore-installed -vv --no-use-pep517 --global-option --rust .
 } || { # catch
-echo "ls rust/target/release/*" && \
-  ls rust/target/release/* && \
-  echo "ls rust/target/release" && \
-  ls rust/target/release
+echo "ls rust/target/$CARGO_BUILD_TARGET/release" && \
+  ls rust/target/$CARGO_BUILD_TARGET/release && \
   find . -name "librusthg.so"
 }
